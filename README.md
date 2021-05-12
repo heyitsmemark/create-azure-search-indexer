d324ec1b6f172bd6c58f090ddab0b89b0a961885
Mark <Mark.Elliott@globalknowledge.net>
2021-05-12 12:12:53 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# create-azure-search-indexer
-A GitHub Action to create a new Indexer within an Azure Cognitive Search instance.
+# `create-azure-search-indexer` GitHub Action
+
+This repository contains an action for use with GitHub Actions, which creates a new Indexer within a specified Azure Cognitive Search instance.
+
+## Usage
+
+Create a new indexer using a local definition file:
+
+```yaml
+- name: Create indexer
+  uses: heyitsmemark/create-azure-search-indexer@main
+  with:
+    azureSearchInstance: plop
+    azureSearchAdminKey: ${{ secrets.AZURE_SEARCH_ADMIN_KEY }}
+    indexerDefinitionFile: ./path/to/indexer.json
+```
+
+Create a new indexer using a remote definition file:
+
+```yaml
+- name: Create indexer 
+  uses: heyitsmemark/create-azure-search-indexer@main
+  with:
+    azureSearchInstance: plop
+    azureSearchAdminKey: ${{ secrets.AZURE_SEARCH_ADMIN_KEY }}
+    indexerDefinitionUri: https://domain.com/path/to/indexer.json
+```
+
+## Samples
+
+Sample workflows are available [here](.github/workflows/)
---
+++ entrypoint.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+API_VERSION=2020-06-30
+
+set -e
+
+if [ -z "$INPUT_AZURESEARCHINSTANCE" ]; then
+  echo "AZURE_SEARCH_INSTANCE is not set."
+  exit 1
+fi
+
+if [ -z "$INPUT_AZURESEARCHADMINKEY" ]; then
+  echo "AZURE_SEARCH_ADMIN_KEY is not set."
+  exit 1
+fi
+
+if [ ! -z "$INPUT_INDEXERDEFINITIONFILE" ]; then
+  echo "Using local definition file."
+  DEFINITION_IS_LOCAL=true
+  DEFINITION_LOCATION=$INPUT_INDEXERDEFINITIONFILE
+fi
+
+if [ ! -z "$INPUT_INDEXERDEFINITIONURI" ]; then
+  echo "Using remote definition file."
+  DEFINITION_IS_LOCAL=false
+  DEFINITION_LOCATION=$INPUT_INDEXERDEFINITIONURI
+fi
+
+if [ -z "$DEFINITION_LOCATION" ]; then
+  echo "Defintion location could not be determined.  Supply one of indexerDefinitionFile or indexerDefinitionUri"
+  exit 1
+fi
+
+if test "$DEFINITION_IS_LOCAL" = true; then
+  if [ -f $DEFINITION_LOCATION ]; then
+    DEFINITION=$(cat $DEFINITION_LOCATION)
+    if test "$?" != "0"; then
+      echo "Unable to read definition file!"
+      exit 1
+    fi
+  else
+    echo "Cannot find definition file '$DEFINITION_LOCATION'"
+    exit 1
+  fi
+else
+  DEFINITION=$(curl --silent $DEFINITION_LOCATION)
+  if test "$?" != "0"; then
+    echo "the curl command failed with: $?"
+    exit 1
+  fi
+fi
+
+echo "Creating new indexer on instance '${INPUT_AZURESEARCHINSTANCE}':"
+echo $DEFINITION | jq
+
+set +e
+RESPONSE=$(curl --fail-with-body -sSD/dev/stderr \
+  --location \
+  --request POST "https://${INPUT_AZURESEARCHINSTANCE}.search.windows.net/indexers?api-version=${API_VERSION}" \
+  --header 'content-type: application/json' \
+  --header "api-key: ${INPUT_AZURESEARCHADMINKEY}" \
+  --data-raw "$DEFINITION")
+
+CURL_EXIT_CODE="$?"
+set -e
+echo $RESPONSE | jq
+
+if test "$CURL_EXIT_CODE" != "0"; then
+  exit 1
+fi
